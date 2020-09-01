dd64c3210b9ecf5b28dc7e0227239eff2de8253b
Rich Bryant <rich.bryant@outlook.com>
2020-09-01 13:11:29 +0100

    Added component

---
+++ UnionComponents/Shared/StringOption.razor.cs
@@ -0,0 +1,17 @@
+﻿using LanguageExt;
+using Microsoft.AspNetCore.Components;
+
+namespace UnionComponents.Shared
+{
+    public partial class StringOption
+    {
+        [Parameter]
+        public Option<string> ContentValue { get; set; }
+
+        [Parameter]
+        public RenderFragment Some { get; set; }
+
+        [Parameter]
+        public RenderFragment None { get; set; }
+    }
+}
