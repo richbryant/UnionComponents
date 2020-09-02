9cbbb7a07fd7fcba1da5dc84cd0c33e6d3f0e569
Rich Bryant <rich.bryant@outlook.com>
2020-09-02 11:37:09 +0100

    reduced component to class file

---
+++ UnionComponents/Shared/Option.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace UnionComponents.Shared
+{
+    public class Option<TValue> : ComponentBase
+    {
+        [Parameter]
+        public TValue OptionValue { get; set; }
+        public LanguageExt.Option<TValue> optionValue => (LanguageExt.Option<TValue>)OptionValue;
+
+        [Parameter]
+        public RenderFragment Some { get; set; }
+
+        [Parameter]
+        public RenderFragment None { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.AddContent(1, optionValue.Match(Some: x => Some, None: () => None));
+        }
+    }
+}
